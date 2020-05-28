e99307da059db08bc216e7f12d8da467dfbd0e54
chirag <cr.tagadiya@gmail.com>
2020-05-28 16:40:17 -0400

    build test

--- test_main.py
+++ test_main.py
@@ -26,6 +26,7 @@ def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
+    
 
 
 def test_auth(client):
@@ -35,7 +36,7 @@ def test_auth(client):
     response = client.post('/auth', 
                            data=json.dumps(body),
                            content_type='application/json')
-
+    assert False
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
